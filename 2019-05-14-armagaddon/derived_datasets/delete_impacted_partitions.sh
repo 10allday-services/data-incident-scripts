399ad972996a82776a01bbf9204cc1ec8d7237a0
Daniel Thorn <dthorn@mozilla.com>
2019-05-17 10:11:02 -0700

    Update delete_impacted_partitions.sh

--- 2019-05-14-armagaddon/derived_datasets/delete_impacted_partitions.sh
+++ 2019-05-14-armagaddon/derived_datasets/delete_impacted_partitions.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
-. ./list_impacted_partitions.sh | gsutil -m rm -Ir
+## This didn't work, we don't know why
+# . ./list_impacted_partitions.sh | gsutil -m rm -I
+
+# in retrospect all of the derived datasets scripts would have been better using this
+bash -ex ./list_impacted_partitions.sh > impacted_partitions.txt
+
+## This didn't work either, also unclear why
+# gsutil -m rm -r $(cat impacted_partitions.txt)
+
+## This worked but was slower than the commands below and also suffered from leaving one file behind on first run
+# cat impacted_partitions.txt | xargs -P10 -n1 gsutil -m rm -r
+
+# This is worked best and was the final commands run
+# run it multiple times because it commonly misses one file
+sed 's,/$,/**,' | gsutil -m rm -I
+sed 's,/$,/**,' | gsutil -m rm -I
+sed 's,/$,/**,' | gsutil -m rm -I
